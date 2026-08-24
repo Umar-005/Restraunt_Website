7a37ee354bff47ee1df64fb093bfaa1d55399f99
Umar-005 <umar00005h@gmail.com>
2026-08-24 23:52:52 +0100

    Build menu API

--- backend/restaurant/models.py
+++ backend/restaurant/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.CharField(max_length=100)
+    available = models.BooleanField(default=True)
